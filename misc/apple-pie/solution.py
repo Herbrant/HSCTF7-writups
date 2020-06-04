2915520f56b2900948972d84f57704c088616cd9
daxcpp <cdavide98carnemolla@gmail.com>
2020-06-04 20:56:36 +0200

    Add apple-pie solution

---
+++ misc/apple-pie/solution.py
@@ -0,0 +1,65 @@
+def reverse_print(val):
+    reversed_val = str(val)[::-1]
+    print(int(reversed_val))
+
+def min_print(val):
+    print(val-1)
+
+#Good luck reading this lol 
+Z = 0
+O =1 
+reverse_print(Z) 
+T = 2
+L = 5*1 
+reverse_print(L)
+
+for i in range(0, 3):
+    print(O)
+
+reverse_print(T)
+
+for i in range(0, 2): 
+    reverse_print(O) 
+
+W = L + 1 
+
+reverse_print(W) 
+
+N = 13*70
+
+N = N+1
+
+reverse_print(N)
+
+min_print(1) 
+P = 0
+K = 0
+
+for i in range(0, 6): 
+    P = P+1
+    K = K+1
+
+
+for i in range(0, 8): 
+    K = K+1
+
+K = K*P
+
+reverse_print(K)
+min_print(2)
+min_print(2)
+
+L = L-5
+reverse_print(L)
+reverse_print(L)
+
+A = 1
+T = 1
+
+for i in range(0, 4): 
+    P = 2*A
+    P = P+1
+    T = T+P
+    A = A + 1
+
+reverse_print(T)
