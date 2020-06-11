4fb0e05fd289faff9c9830d5276e571c5883ac98
daxcpp <cdavide98carnemolla@gmail.com>
2020-06-11 15:24:20 +0200

    Update apple-pie solution

--- misc/apple-pie/solution.py
+++ misc/apple-pie/solution.py
@@ -1,37 +1,38 @@
 def reverse_print(val):
-    reversed_val = str(val)[::-1]
-    print(int(reversed_val))
+    return str(val)[::-1]
 
 def min_print(val):
-    print(val-1)
+    return str(val-1)
+
+output = ""
 
 #Good luck reading this lol 
 Z = 0
 O =1 
-reverse_print(Z) 
+output += reverse_print(Z) 
 T = 2
 L = 5*1 
-reverse_print(L)
+output += reverse_print(L)
 
 for i in range(0, 3):
-    print(O)
+    output += reverse_print(O)
 
-reverse_print(T)
+output += reverse_print(T)
 
 for i in range(0, 2): 
     reverse_print(O) 
 
 W = L + 1 
 
-reverse_print(W) 
+output += reverse_print(W) 
 
 N = 13*70
 
 N = N+1
 
-reverse_print(N)
+output += reverse_print(N)
 
-min_print(1) 
+output += min_print(1) 
 P = 0
 K = 0
 
@@ -45,13 +46,13 @@ for i in range(0, 8):
 
 K = K*P
 
-reverse_print(K)
+output += reverse_print(K)
 min_print(2)
 min_print(2)
 
 L = L-5
-reverse_print(L)
-reverse_print(L)
+output += reverse_print(L)
+output += reverse_print(L)
 
 A = 1
 T = 1
@@ -62,4 +63,6 @@ for i in range(0, 4):
     T = T+P
     A = A + 1
 
-reverse_print(T)
+output += reverse_print(T)
+
+print(output)
